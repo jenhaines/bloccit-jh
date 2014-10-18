eaf4b6e4e113cd2121cd1a0e8a131f529dd887eb
Jen Haines <motochick67@gmail.com>
2014-10-17 21:03:19 -0700

    Added Validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,3 +3,13 @@ module ApplicationHelper
     p "Jen Haines"
   end
 end
+
+ module ApplicationHelper
+   def form_group_tag(errors, &block)
+     if errors.any?
+       content_tag :div, capture(&block), class: 'form-group has-error'
+     else
+       content_tag :div, capture(&block), class: 'form-group'
+     end
+   end
+ end
