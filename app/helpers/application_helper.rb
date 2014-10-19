bc9f078500673f94ce0f20d84a6bf00252a7472c
Jen Haines <motochick67@gmail.com>
2014-10-18 21:00:37 -0700

    Different Solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,11 @@
+
 module ApplicationHelper
-  def my_name
-    p "Jen Haines"
+
+  def form_tag_group(errors)
+    if errors.any?
+      'form-group has-error'
+    else
+      'form-group'
+    end
   end
 end
-
- module ApplicationHelper
-   def form_group_tag(errors, &block)
-     if errors.any?
-       content_tag :div, capture(&block), class: 'form-group has-error'
-     else
-       content_tag :div, capture(&block), class: 'form-group'
-     end
-   end
- end
