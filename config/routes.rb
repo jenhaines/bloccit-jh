1b084a2c4318584781649bb70f4f99ddc0cb8c5f
Jen Haines <motochick67@gmail.com>
2014-10-22 13:24:06 -0700

    Delete Added

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   devise_for :users
     resources :users, only: [:update]
 
-   resources :topics do
-     resources :posts, except: [:index] do
-        resources :comments,  only: [:create]
-      end
-   end
+resources :topics do
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
+    end
+  end
 
   get 'about' => 'welcome#about'
 
