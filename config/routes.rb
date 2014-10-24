dad0333141d7aecd9d45addb482868f411fb3e5c
Jen Haines <motochick67@gmail.com>
2014-10-24 14:42:25 -0700

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
     resources :users, only: [:update]
 
   resources :topics do
-    resources :posts, shallow: true, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
 
   get 'about' => 'welcome#about'
 
-  root to: 'welcome#index'
+    root to: 'welcome#index'
 
-end
+ end
