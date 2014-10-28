a48502e11a644fdf911a34fdc3df0fe0f354b168
Jen Haines <motochick67@gmail.com>
2014-10-27 21:26:09 -0700

    Interlude

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,17 +2,23 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-   validates :body, length: { minimum: 5 }
-   validates :body, presence: true
-   validates :user_id, presence: true
+  validates :body, length: { minimum: 5 }
+  validates :body, presence: true
+  validates :user_id, presence: true
 
-   after_create :send_favorite_emails
+  after_create :send_favorite_emails
 
-   private
+  private
 
-   def send_favorite_emails
-     post.favorites.each do |favorite|
-       FavoriteMailer.new_comment(favorite.user, post, self).deliver
-     end
-   end
+  def send_favorite_emails
+    post.favorites.each do |favorite|
+      if should_receive_update_for?(favorite)
+        FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+      end
+    end
+  end
+
+  def should_receive_update_for?(favorite)
+    user_id != favorite.user_id && favorite.user.email_favorites?
+  end
 end
