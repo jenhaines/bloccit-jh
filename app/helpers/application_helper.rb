8c9479474dcc1cddba4710e709ff7b22cbb60f12
Jen Haines <motochick67@gmail.com>
2014-10-30 13:45:39 -0700

    Refactored Vote Arrows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,14 @@
     'Jen Haines'
   end
 
+  def vote_link_classes(post, direction)
+    if direction == 'up'
+      "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+    else
+      "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+    end
+  end
+
    def form_group_tag(errors, &block)
      if errors.any?
        content_tag :div, capture(&block), class: 'form-group has-error'
